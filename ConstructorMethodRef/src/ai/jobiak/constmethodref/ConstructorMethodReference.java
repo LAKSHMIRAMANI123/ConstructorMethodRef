59bc7f362c8ec8edc269ad46b39b9f9db96271b0
ramani <lakshmiramanitupakula@gmail.com>
2022-05-17 10:53:38 +0530

    Done

---
+++ ConstructorMethodRef/src/ai/jobiak/constmethodref/ConstructorMethodReference.java
@@ -0,0 +1,21 @@
+package ai.jobiak.constmethodref;
+
+interface Messageable{  
+    Message getMessage(String msg);  
+}  
+class Message{  
+    Message(String msg){  
+        System.out.print(msg);  
+    }  
+}  
+
+public class ConstructorMethodReference {
+
+	public static void main(String[] args) {
+		
+		 Messageable hello = Message::new;  
+	        hello.getMessage("Hello");  
+
+	}
+
+}
